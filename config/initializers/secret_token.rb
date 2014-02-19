fda1d911a3242ba2066fa6923ffe3faf159bc73c
Ben Blackburn <benjamin.blackburn@gmail.com>
2014-02-19 14:16:49 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TraxterIo::Application.config.secret_key_base = '9f738f80d7c555708a50c4315e925e4bb19cff3d947b55669073825140a36cf2b732687eca897c5c56c67738127afeb07d3015d751f9b472518f5f8431c10309'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TraxterIo::Application.config.session_store :cookie_store, key: '_traxter_io_session'
